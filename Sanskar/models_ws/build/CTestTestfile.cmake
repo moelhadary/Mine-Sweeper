b38376ea1d6fb512835f916b1f5537e5b456d0b0
sanskarsoni88 <111488987+sanskarsoni88@users.noreply.github.com>
2024-02-12 15:15:56 -0800

    Delete Sanskar/models_ws directory
    old urdf

--- Sanskar/models_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sanskar_/models_ws/src
-# Build directory: /home/sanskar_/models_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("my_robot_description")
